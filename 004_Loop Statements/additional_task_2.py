a5f39fcdfaa7d9612701900ff0b714d8a7f3f393
Ozerov Alex <sasha.ozerov98@gmail.com>
2021-04-14 13:44:04 +0300

    deleted

--- 004_Loop Statements/additional_task_2.py
+++
@@ -1,6 +0,0 @@
-while True:
-    input_data = input("Введите число: ")
-    if input_data.isdigit() and int(input_data) >= 10 and int(input_data) <= 50:
-        break
-    else:
-        print("Попробуйте еще")
